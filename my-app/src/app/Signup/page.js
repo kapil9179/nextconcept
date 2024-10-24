61c044ae8b09dc73350fa2eec921e6e465f5e0d5
Kapil Dev Pandey <138851913+kapil9179@users.noreply.github.com>
2024-10-25 00:21:57 +0530

    Update signup page

--- my-app/src/app/Signup/page.js
+++ my-app/src/app/Signup/page.js
@@ -6,11 +6,15 @@ import Login from '../_features/Login';
 
   function SignUpPage() {
   const [count,setcount] = useState(0);
+    const handlecountclick = ()=>{
+      setcount((prev)=>prev+1)
+    }
   return (
     <div>
         <Signup
         />
         <Login/>
+     <button onclick={handleclick}>click<button/>
     </div>
   )
 }
